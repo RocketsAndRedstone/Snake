47df331195a406ba87c0b48d61573d8560a415bd
RocketsAndRedstone <119061890+RocketsAndRedstone@users.noreply.github.com>
2023-03-22 14:16:07 -0400

    Update Snake.py

--- Snake.py
+++ Snake.py
@@ -10,7 +10,7 @@ apple = turtle.Turtle()
 apple.ht()
 apple.penup()
 apple.shape("circle")
-apple.color("Red")#creates the snake that you are controling, the apple to be consumed, and the playing board
+apple.color("Red")#creates the snake that you are controlling, the apple to be consumed, and the playing board
 def main():
     playing = True
     tailCords = []
@@ -27,7 +27,7 @@ def main():
         if (pencil.xcor() - colisionArea <= applePosition[0]) and (pencil.xcor() + colisionArea >= applePosition[0]) and (pencil.ycor() + colisionArea >= applePosition[1]) and (pencil.ycor() - colisionArea <= applePosition[1]):
             applePlaced = False#detects to see if the head of the snake is within a set area around the apple
             score += 1
-            tailCords.append(pencil.xcor())#used for the colision detection between the head and tail for the snake
+            tailCords.append(pencil.xcor())#used for the collision detection between the head and tail for the snake
             tailCords.append(pencil.ycor())
             pencil.fd(5)
             pencil.stamp()
@@ -44,24 +44,24 @@ def main():
                 tailCords.append(pencil.ycor())
                 pencil.fd(5)
                 for i in range(2):
-                    del tailCords[0]#removes the tail's cordanents for the segment that was just deleted on screen
+                    del tailCords[0]#removes the tail's coordinates for the segment that was just deleted on screen
                 pencil.stamp()
                 pencil.clearstamps(1)        
         turtle.onkey(up , "w")
         turtle.onkey(left , "a")
         turtle.onkey(down , "s")
         turtle.onkey(right , "d")
-        turtle.listen()#gets the WASD input to controll the snake
+        turtle.listen()#gets the WASD input to control the snake
         for i in range(int((len(tailCords) / 2))):
             if (pencil.xcor() - colisionArea1 <= tailCords[i]) and (pencil.xcor() + colisionArea1 >= tailCords[i] and pencil.ycor() - colisionArea1 <= tailCords[i + 1]) and (pencil.ycor() + colisionArea1 >= tailCords[i + 1]):
-                deathScreen(score)#Detects if the snake head has colided with any part of the tail, if so, it displays the amount of apples eaten
+                deathScreen(score)#Detects if the snake head has collided with any part of the tail, if so, it displays the amount of apples eaten
                 playing = False
-        if pencil.xcor()>= 95 or pencil.xcor()<= -95 or pencil.ycor()<= -95 or pencil.ycor()>= 95:#detects if the head of the snake has colided with any of the walls
+        if pencil.xcor()>= 95 or pencil.xcor()<= -95 or pencil.ycor()<= -95 or pencil.ycor()>= 95:#detects if the head of the snake has collided with any of the walls
             deathScreen(score)
             playing = False
                   
 def placeApple():
-    apple.clearstamps(1)#removes the privious apple so that it is fully consumed
+    apple.clearstamps(1)#removes the previous apple so that it is fully consumed
     appleSpotX = random.randint(-90 , 90)
     appleSpotY = random.randint(-90 , 90)
     apple.goto(appleSpotX , appleSpotY)#generates and places the random spot for the apple
